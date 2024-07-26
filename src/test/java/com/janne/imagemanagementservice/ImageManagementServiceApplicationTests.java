5d555f3260c6b50d7adf3b849348b5ac723789d2
janne6565 <jabbekeipert@gmail.com>
2024-07-26 21:59:29 +0200

    initial push

---
+++ src/test/java/com/janne/imagemanagementservice/ImageManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janne.imagemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
