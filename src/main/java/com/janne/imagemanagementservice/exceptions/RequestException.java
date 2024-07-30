ffe0f6163d74b978f30bccc29d9b942e79ad25b1
Janne <60547730+Janne6565@users.noreply.github.com>
2024-07-30 18:24:41 +0200

    implement image deletion endpoint (#2)

---
+++ src/main/java/com/janne/imagemanagementservice/exceptions/RequestException.java
@@ -0,0 +1,14 @@
+package com.janne.imagemanagementservice.exceptions;
+
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestException extends RuntimeException {
+    private String reason;
+    private int code;
+    private String message;
+}
